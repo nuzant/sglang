95b2439a7053529e2336df5604d585b0a9c47da1
晓雷 <meizhiyu.mzy@antgroup.com>
2025-09-08 14:51:02 +0800

    profile

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -253,7 +253,7 @@ class ModelRunner:
         self._model_update_group = {}
 
     def initialize(self, min_per_gpu_memory: float):
-        print("[debug] test sglang patch")
+        logger.info("[Debug] SGLang v0.5.1.post1 is patched with customized weight loading.")
         server_args = self.server_args
 
         self.memory_saver_adapter = TorchMemorySaverAdapter.create(
@@ -778,6 +778,8 @@ class ModelRunner:
         self.model_config.model_path = model_path
         load_config = LoadConfig(load_format=load_format)
 
+        logger.info(f"[Debug] `update_weights_from_disk` from model_path={model_path}")
+        tik = time.perf_counter()
         # Only support DefaultModelLoader for now
         loader = get_model_loader(load_config)
         if not isinstance(loader, DefaultModelLoader):
@@ -811,6 +813,9 @@ class ModelRunner:
                 iter = get_weight_iter(self.model_config)
                 self.model = model_load_weights(self.model, iter)
                 return False, message
+        
+        tok = time.perf_counter()
+        logger.info(f"[Debug] `update_weights_from_disk` finished in {tok - tik:.2f} seconds")
 
         self.model = model
         self.server_args.model_path = model_path
