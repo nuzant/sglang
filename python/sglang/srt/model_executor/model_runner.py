c97fdbc84e94234c7ee436290870051197e66bba
晓雷 <meizhiyu.mzy@antgroup.com>
2025-09-09 13:54:56 +0800

    debug

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -797,7 +797,7 @@ class ModelRunner:
             return iter
 
         def model_load_weights(model, iter):
-            DefaultModelLoader.load_weights_and_postprocess(model, iter, target_device, load_config)
+            DefaultModelLoader.load_weights_and_postprocess(model, iter, target_device, load_config=load_config)
             return model
 
         with set_default_torch_dtype(self.model_config.dtype):
