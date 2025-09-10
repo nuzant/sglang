eb53be6e5c1da77b69580c9af5faa334d0f0bb4b
晓雷 <meizhiyu.mzy@antgroup.com>
2025-09-10 17:48:46 +0800

    .

--- python/sglang/srt/models/qwen3_moe.py
+++ python/sglang/srt/models/qwen3_moe.py
@@ -808,7 +808,7 @@ class Qwen3MoeForCausalLM(nn.Module):
                     # all_slices[name] = f.get_slice(name)
                     all_slices[name] = f.get_tensor(name)
 
-        print(f"[Debug] expert_param_mappings={self.expert_params_mapping}", flush=True)
+        # print(f"[Debug] expert_param_mappings={self.expert_params_mapping}", flush=True)
 
         print(f"[Debug] Slice names={list(all_slices.keys())}", flush=True)
         print(f"[Debug] Local names={local_names}", flush=True)
