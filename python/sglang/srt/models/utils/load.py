a88c4dbd96e4f14630ef33324573176790036213
晓雷 <meizhiyu.mzy@antgroup.com>
2025-09-11 14:28:03 +0800

    fix file grouping algorithm

--- python/sglang/srt/models/utils/load.py
+++ python/sglang/srt/models/utils/load.py
@@ -76,49 +76,96 @@ def load_weights_with_hf_path_fast(
                 if "model.layers.31.mlp.experts" in local_name:
                     print(f"[Debug] filename for {local_name}: {filename}")
 
+    # Convert filename lists to sets for faster intersection operations
+    # weight_files = {weight: set(files) for weight, files in weight_dict.items()}
+    # weight_names = list(weight_files.keys())
+    
+    # Build adjacency graph: weight_names that share files are connected
+    conflicts = {}
+    for weight in local_names:
+        conflicts[weight] = set()
+    
+    for i, weight1 in enumerate(local_names):
+        for weight2 in local_names[i+1:]:
+            # If two weights share any files, they conflict
+            if local_to_file_map[weight1] & local_to_file_map[weight2]:
+                conflicts[weight1].add(weight2)
+                conflicts[weight2].add(weight1)
+    
+    # Greedy graph coloring algorithm
+    grouped_local_names = []
+    assigned = set()
+    
+    for weight in local_names:
+        if weight in assigned:
+            continue
+            
+        # Try to add to existing group
+        placed = False
+        for group in grouped_local_names:
+            # Check if weight conflicts with any member of this group
+            if not any(member in conflicts[weight] for member in group):
+                group.append(weight)
+                assigned.add(weight)
+                placed = True
+                break
+        
+        # If couldn't place in existing group, create new group
+        if not placed:
+            grouped_local_names.append([weight])
+            assigned.add(weight)
+
+    # Map each group to the union of their required files
+    grouped_filenames = []
+    for group in grouped_local_names:
+        files = set()
+        for weight in group:
+            files.update(local_to_file_map[weight])
+        grouped_filenames.append(list(files))
+
     # Allocate local weight name into bins, where each bin access independent files
     # Then we can use multiple threads to concurrently load each bin's parameters
-    weight_name_bins = {}
-    file_name_to_bin_index = {}
-    bin_index = 0
-    for local_name, filenames in local_to_file_map.items():
-        if not all(filename in file_name_to_bin_index for filename in filenames):
-            # Some required filenames doesn't have existing bins
-            # Allocate a new bin, and merge all previous bins into this new bin
-            weight_name_bins[bin_index] = [local_name]
-            for filename in filenames:
-                if filename in file_name_to_bin_index:
-                    i = file_name_to_bin_index.pop(filename) # bin
-                    if i in weight_name_bins:
-                        weight_name_bins[bin_index] += weight_name_bins.pop(i)
-                file_name_to_bin_index[filename] = bin_index
-            bin_index += 1
-        else:
-            # All required filenames have existing bins
-            # Use the head bin as the master bin, and merge all other bins into the master bin
-            filenames = list(filenames)
-            head_i = file_name_to_bin_index[filenames[0]]
-            weight_name_bins[head_i].append(local_name)
-            if not all(
-                file_name_to_bin_index[filename] == head_i for filename in filenames
-            ):
-                # merge to head bin
-                for filename in filenames[1:]:
-                    if file_name_to_bin_index[filename] != head_i:
-                        i = file_name_to_bin_index.pop(filename)
-                        if i in weight_name_bins:
-                            weight_name_bins[head_i] += weight_name_bins.pop(i)
-                        file_name_to_bin_index[filename] = head_i
+    # weight_name_bins = {}
+    # file_name_to_bin_index = {}
+    # bin_index = 0
+    # for local_name, filenames in local_to_file_map.items():
+    #     if not all(filename in file_name_to_bin_index for filename in filenames):
+    #         # Some required filenames doesn't have existing bins
+    #         # Allocate a new bin, and merge all previous bins into this new bin
+    #         weight_name_bins[bin_index] = [local_name]
+    #         for filename in filenames:
+    #             if filename in file_name_to_bin_index:
+    #                 i = file_name_to_bin_index.pop(filename) # bin
+    #                 if i in weight_name_bins:
+    #                     weight_name_bins[bin_index] += weight_name_bins.pop(i)
+    #             file_name_to_bin_index[filename] = bin_index
+    #         bin_index += 1
+    #     else:
+    #         # All required filenames have existing bins
+    #         # Use the head bin as the master bin, and merge all other bins into the master bin
+    #         filenames = list(filenames)
+    #         head_i = file_name_to_bin_index[filenames[0]]
+    #         weight_name_bins[head_i].append(local_name)
+    #         if not all(
+    #             file_name_to_bin_index[filename] == head_i for filename in filenames
+    #         ):
+    #             # merge to head bin
+    #             for filename in filenames[1:]:
+    #                 if file_name_to_bin_index[filename] != head_i:
+    #                     i = file_name_to_bin_index.pop(filename)
+    #                     if i in weight_name_bins:
+    #                         weight_name_bins[head_i] += weight_name_bins.pop(i)
+    #                     file_name_to_bin_index[filename] = head_i
     
-    print(f"[Debug] weight_name_bins={weight_name_bins}")
-    print(f"[Debug] file_name_to_bin_index={file_name_to_bin_index}")
+    # print(f"[Debug] weight_name_bins={weight_name_bins}")
+    # print(f"[Debug] file_name_to_bin_index={file_name_to_bin_index}")
 
-    bin_index_to_file_names = defaultdict(list)
-    for filename, bin_index in file_name_to_bin_index.items():
-        bin_index_to_file_names[bin_index].append(filename)
+    # bin_index_to_file_names = defaultdict(list)
+    # for filename, bin_index in file_name_to_bin_index.items():
+    #     bin_index_to_file_names[bin_index].append(filename)
 
-    grouped_local_names = list(weight_name_bins.values())
-    grouped_filenames = list(bin_index_to_file_names[i] for i in weight_name_bins)
+    # grouped_local_names = list(weight_name_bins.values())
+    # grouped_filenames = list(bin_index_to_file_names[i] for i in weight_name_bins)
 
     print(f"[Debug] len(grouped_local_names) = {len(grouped_local_names)} len(grouped_filenames) = {len(grouped_filenames)}")
     if max_workers is None:
